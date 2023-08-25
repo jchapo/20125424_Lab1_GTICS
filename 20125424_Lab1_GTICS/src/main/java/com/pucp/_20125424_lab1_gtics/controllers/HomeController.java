76baf6bfb40be18aba24bc09d7217ed1a7d205b8
Juan Aasdfasdf <jchaponan@pucp.pe>
2023-08-24 21:50:35 -0500

    Se agregó validación para mostrar el logo

--- 20125424_Lab1_GTICS/src/main/java/com/pucp/_20125424_lab1_gtics/controllers/HomeController.java
+++ 20125424_Lab1_GTICS/src/main/java/com/pucp/_20125424_lab1_gtics/controllers/HomeController.java
@@ -1,15 +1,28 @@
 package com.pucp._20125424_lab1_gtics.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/principal",method = RequestMethod.GET)
-    public String empleado(){
+
+
+
+    @RequestMapping("/principal")
+    public String principalpucp(Model model,  @RequestParam(value = "code" , required = false) String pucp2 )
+    {
+        try{
+            if (pucp2.equals("PUCP")){
+                Integer pucp = 1;
+                model.addAttribute("pucp", pucp);
+            }
+        }catch (Exception  e){
+            return "principal";
+        }
+
+
         return "principal";
     }
 }
